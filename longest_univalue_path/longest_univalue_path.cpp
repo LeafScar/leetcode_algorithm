c5f4be82f796325dc74d4d8db458d6912c2d911d
LeafScar <taosihao@126.com>
2019-03-26 21:59:10 +0800

    修改

--- longest_univalue_path/longest_univalue_path.cpp
+++ longest_univalue_path/longest_univalue_path.cpp
@@ -1,14 +1,34 @@
-Definition for a binary tree node.
-* struct TreeNode {
-	*int val;
-	*TreeNode *left;
-	*TreeNode *right;
-	*TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-	*
+ #include <algorithm>
+using namespace std;
+ struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	
 };
 class Solution {
 public:
+	int maxLen = 0;
 	int longestUnivaluePath(TreeNode* root) {
+		if (root == nullptr)
+			return 0;
 
+		getMaxLen(root, root->val);
+		return maxLen;
+	}
+
+	int getMaxLen(TreeNode* root, int val)
+	{
+		if (root == nullptr)
+			return 0;
+
+		int left = getMaxLen(root->left, root->val);
+		int right = getMaxLen(root->right, root->val);
+		maxLen = max(maxLen, left + right);
+		if (root->val == val)
+			return max(left, right) + 1;
+
+		return 0;
 	}
 };
